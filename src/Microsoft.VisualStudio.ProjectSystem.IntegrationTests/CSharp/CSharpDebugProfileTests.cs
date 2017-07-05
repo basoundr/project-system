de3c1b13de318c4ece33df696bf34b8f6c909b58
Balaji Soundrarajan <basoundr@microsoft.com>
2017-07-05 15:59:35 -0700

    More tests

--- src/Microsoft.VisualStudio.ProjectSystem.IntegrationTests/CSharp/CSharpDebugProfileTests.cs
+++ src/Microsoft.VisualStudio.ProjectSystem.IntegrationTests/CSharp/CSharpDebugProfileTests.cs
@@ -32,7 +32,7 @@ namespace Microsoft.VisualStudio.CSharp
 
 </Project>");
 
-            VisualStudio.SolutionExplorer. SaveAll();
+            VisualStudio.SolutionExplorer.SaveAll();
             //VisualStudio.Dialog.VerifyOpen("Project Modification Detected");
             //VisualStudio.SendKeys.Send(VirtualKey.R);
 
@@ -43,6 +43,7 @@ namespace Microsoft.VisualStudio.CSharp
             var fileName = "Program.cs";
             VisualStudio.SolutionExplorer.OpenFile(Project, fileName);
             VisualStudio.Editor.SetText(@"using System;
+using System.IO;
 
 namespace TestProj
 {
@@ -52,6 +53,7 @@ namespace TestProj
         {
             var value = args[0];
             var path = System.Reflection.Assembly.GetEntryAssembly().Location;
+            var tfm = Path.GetFileName(Path.GetDirectoryName(path));
             Console.WriteLine(path);
         }
     }
@@ -71,17 +73,42 @@ namespace TestProj
   }
 }";
             VisualStudio.SolutionExplorer.AddFile(Project, "Properties\\launchSettings.json", launchSettingsContent);
-
             VisualStudio.Workspace.WaitForAsyncOperations(FeatureAttribute.Workspace);
 
             VisualStudio.Debugger.SetBreakPoint(fileName, "Console.WriteLine(path);");
             VisualStudio.Debugger.Go(waitForBreakMode: true);
 
-            VisualStudio.LocalsWindow.Verify.CheckEntry("value", "String", "ProfileOne");
+            VisualStudio.LocalsWindow.Verify.CheckEntry("value", "string", "\"ProfileOne\"");
+            VisualStudio.LocalsWindow.Verify.CheckEntry("tfm", "string", "\"netcoreapp1.1\"");
             VisualStudio.Debugger.Go(waitForBreakMode: true);
 
-            var availableCommands = VisualStudio.GetAvailableCommands();
+            // Change the profile and the target framework
+            var csprojectUserContent = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<Project ToolsVersion=""15.0"" xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+  <PropertyGroup>
+    <ActiveDebugFramework>net461</ActiveDebugFramework>
+    <ActiveDebugProfile>Profile2</ActiveDebugProfile>
+  </PropertyGroup>
+  <PropertyGroup Condition=""'$(Configuration)|$(TargetFramework)|$(Platform)'=='Debug|netcoreapp1.1|AnyCPU'"">
+    <DebuggerFlavor>ProjectDebugger</DebuggerFlavor>
+  </PropertyGroup>
+</Project>";
+            VisualStudio.SolutionExplorer.AddFile(Project, "TestProj.csproj.user", csprojectUserContent);
+            VisualStudio.Workspace.WaitForAsyncOperations(FeatureAttribute.Workspace);
 
+            // Unload and Reload the project to detect the .user file
+            VisualStudio.SolutionExplorer.UnloadProject(Project);
+            VisualStudio.SolutionExplorer.ReloadProject(Project);
+            VisualStudio.Workspace.WaitForAsyncOperations(FeatureAttribute.SolutionCrawler);
+
+            // Start
+            VisualStudio.Debugger.Go(waitForBreakMode: true);
+
+            VisualStudio.LocalsWindow.Verify.CheckEntry("value", "string", "\"ProfileTwo\"");
+            VisualStudio.LocalsWindow.Verify.CheckEntry("tfm", "string", "\"net461\"");
+            VisualStudio.Debugger.Go(waitForBreakMode: true);
+
+            //var availableCommands = VisualStudio.GetAvailableCommands();
         }
     }
 }
